52314db7be91c19d79b845ea76c830ada71c5735
XNMEPA <magistr00@gmail.com>
2020-03-17 23:57:42 +0300

    Проба пера. Spring.

---
+++ src/main/java/ru/malychev/animals/Animals.java
@@ -0,0 +1,21 @@
+package ru.malychev.animals;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.malychev.animals.entities.*;
+
+public class Animals {
+	public static void main(String[] args) {
+		ApplicationContext context =
+				new AnnotationConfigApplicationContext("ru.malychev.animals.entities");
+
+		Cat cat = context.getBean(Cat.class);
+		Dog dog = (Dog) context.getBean("dog");
+		Parrot parrot = context.getBean("parrot-kesha", Parrot.class);
+
+		System.out.println(cat.getName());
+		System.out.println(dog.getName());
+		System.out.println(parrot.getName());
+
+	}
+}
---
+++ src/main/java/ru/malychev/animals/entities/Parrot.java
@@ -0,0 +1,16 @@
+package ru.malychev.animals.entities;
+
+import org.springframework.stereotype.Component;
+
+@Component("parrot-kesha")
+public class Parrot {
+	private String name = "Kesha";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
